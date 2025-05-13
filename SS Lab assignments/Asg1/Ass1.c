4981a4b1046e645120d257745972b678499854e6
Nikita Bhoge <167703036+bhogenikita1@users.noreply.github.com>
2025-05-13 23:11:50 +0530

    Add files via upload

---
+++ SS Lab assignments/Asg1/Ass1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+//this is c code
+int a=10;
+float b=9.0;
+int mul=a*b;
+string name="sakshiaware31@gmail.com";
+printf("\nFloat value is:%f",b);
+printf("\nInteger value is:%d",a);
+printf("\nAddition is :%d",a+a);
+printf("\nEmail:%s",name);
+/*here i have used for loop*/
+
+for(int i=0;i<9;i++)
+{
+printf("%d",i);
+}
+return 0;
+}
+
